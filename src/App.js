e3e0ad47591a8dde721913f37e185cc4eea892d9
MasudRana15924 <masudrana15924@outlook.com>
2023-11-17 23:01:53 +0600

    Create Navbar for Frontend UI

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import {
 } from "react-router-dom";
 import Register from './feature/auth/Register';
 import Login from './feature/auth/Login';
+import Navbar from './feature/common/Navbar';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar></Navbar>
         <Routes>
              <Route path="/register" element={<Register></Register>} />
              <Route path="/login" element={<Login></Login>} />
