d42ac5812e6332fe9b56532e92a50adde6630893
MasudRana15924 <masudrana15924@outlook.com>
2023-11-17 14:00:34 +0600

    Create boilerplate template for Learnify react app

--- src/feature/auth/Login.js
+++ src/feature/auth/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Button, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
 const Login = () => {
     return (
         <div className=" flex flex-col items-center justify-center mt-12 lg:mt-52">
-            <form action="" className="space-y-6 py-6 mt-6" >
-
+            <form action="" className="space-y-6 py-6 mt-6 border rounded p-4 shadow-sm" >
+                <p className="text-2xl text-start">Login </p>
                 <TextField id="outlined-basic" label="Email" variant="outlined" className="w-full py-3 px-6" />
                 <TextField id="outlined-basic" label="Password" variant="outlined" className="w-full py-3 px-6" />
-                <Button variant="contained" className="w-full py-3 px-6 h-12" >Login</Button>
+                <Link>
+                    <p className="text-start text-blue-500 mt-4">Forgot password ?</p>
+                </Link>
+                <Button variant="contained" className="w-full py-3 px-6 h-12 " >Login</Button>
 
+                <p className="text-center text-gray-900 mt-4">Dont have an account ? <span className="text-blue-500"> <Link to="/register">
+
+                    Register</Link></span></p>
             </form>
         </div>
     );
--- src/feature/auth/Register.js
+++ src/feature/auth/Register.js
@@ -1,15 +1,18 @@
 import { Button, TextField } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div className=" flex flex-col items-center justify-center mt-12 lg:mt-52">
-            <form action="" className="space-y-6 py-6 mt-6" >
-
+            <form action="" className="space-y-6 py-6 mt-6 border rounded p-4 shadow-sm" >
+            <p className="text-2xl text-start">Register</p>
                 <TextField id="outlined-basic" label="Email" variant="outlined" className="w-full py-3 px-6" />
                 <TextField id="outlined-basic" label="Password" variant="outlined" className="w-full py-3 px-6" />
                 <Button variant="contained" className="w-full py-3 px-6 h-12" >Register</Button>
+                <p className="text-center text-gray-900 mt-4">Already have an account ? <span className="text-blue-500"> <Link to="/login">
 
+                    Login</Link></span></p>
             </form>
         </div>
     );
