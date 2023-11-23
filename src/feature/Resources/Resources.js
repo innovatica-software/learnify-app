bba5429d06d7258007e8d9ffc9649568c05b2d60
Nahid Hasan <nahidjc80@gmail.com>
2023-11-23 19:15:47 +0600

    button size updated

--- src/feature/Resources/Resources.js
+++ src/feature/Resources/Resources.js
@@ -1,36 +1,59 @@
 import React, { useEffect } from "react";
-import './Resources.css'
+import "./Resources.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchResources } from "../../state/reducers/resources/resourcesSlice";
 import Loader from "../../components/Loader/Loader";
-import { Button } from "@mui/material";
+import { Grid, Button } from "@mui/material";
 const Resources = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchResources());
   }, [dispatch]);
-  const { isLoading, resource } = useSelector(state => state.resources);
+  const { isLoading, resource } = useSelector((state) => state.resources);
   return (
-
-    <div >
-      {
-        isLoading ? <div className="flex flex-1 justify-center items-center mt-16">
+    <div style={{ marginBottom: "20px" }}>
+      {isLoading ? (
+        <div className="flex flex-1 justify-center items-center mt-16">
           <Loader></Loader>
-        </div> : <div className="grid md:grid-cols-2 gap-4 w-3/4 lg:w-3/4 xl:w-2/4 mx-auto mt-12">
-          {resource.map(rcs => (
+        </div>
+      ) : (
+        <div
+          style={{ borderRadius: "5px" }}
+          className="grid md:grid-cols-2 gap-4 w-3/4 lg:w-3/4 xl:w-2/4 mx-auto mt-12"
+        >
+          {resource.map((rcs) => (
             <div key={rcs.id} className="resources-card ">
-              <img src={rcs.thumbnail} alt={rcs.title} className="w-full h-60" />
-              <h3 className="text-start text-xl  m-4 font-bold text-white ">{rcs.title}</h3>
-              <h3 className="text-white m-4 text-start font-medium">{rcs.description}</h3>
-              <a href={rcs.link} target="_blank" rel="noopener noreferrer" className="link-btn " >
-                <Button style={{
-                  marginTop: 20, marginBottom: 20, color: "white"
-                }} >SEE MORE</Button>
-              </a>
+              <img
+                src={rcs.thumbnail}
+                alt={rcs.title}
+                className="w-full h-60"
+              />
+              <h3 className="text-start text-xl  m-4 font-bold text-white ">
+                {rcs.title}
+              </h3>
+              <h3 className="text-white m-4 text-start font-medium">
+                {rcs.description}
+              </h3>
+              <Grid container justifyContent="flex-start">
+                <Button
+                  href={rcs.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    color: "white",
+                    border: "1px solid white",
+                    borderRadius: "5px",
+                    margin: "10px",
+                    padding: "10px",
+                  }}
+                >
+                  WATCH
+                </Button>
+              </Grid>
             </div>
           ))}
         </div>
-      }
+      )}
     </div>
   );
 };
