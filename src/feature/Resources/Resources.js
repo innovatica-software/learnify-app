37b016440d46a3852fd68a4febc7b419cfb96a70
MasudRana15924 <masudrana15924@outlook.com>
2023-11-23 17:25:30 +0600

    resources page design like same babbel resources page

--- src/feature/Resources/Resources.js
+++ src/feature/Resources/Resources.js
@@ -3,6 +3,7 @@ import './Resources.css'
 import { useDispatch, useSelector } from "react-redux";
 import { fetchResources } from "../../state/reducers/resources/resourcesSlice";
 import Loader from "../../components/Loader/Loader";
+import { Button } from "@mui/material";
 const Resources = () => {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -15,17 +16,17 @@ const Resources = () => {
       {
         isLoading ? <div className="flex flex-1 justify-center items-center mt-16">
           <Loader></Loader>
-        </div> : <div className="grid md:grid-cols-2 gap-4 w-3/4 lg:w-2/4 mx-auto mt-12">
+        </div> : <div className="grid md:grid-cols-2 gap-4 w-3/4 lg:w-3/4 xl:w-2/4 mx-auto mt-12">
           {resource.map(rcs => (
-            <div key={rcs.id} className="resources-card mt-4 w-full border">
-              <img src={rcs.thumbnail} alt={rcs.title} className="w-full h-48" />
-              <div>
-                <h3 className="text-start text-xl  m-4 font-bold text-white ">{rcs.title}</h3>
-                <h3 className="text-white m-4 text-start font-medium">{rcs.description}</h3>
-                <a href={rcs.link} target="_blank" rel="noopener noreferrer">
-                  <button className="recources-btn mt-8">See More</button>
-                </a>
-              </div>
+            <div key={rcs.id} className="resources-card ">
+              <img src={rcs.thumbnail} alt={rcs.title} className="w-full h-60" />
+              <h3 className="text-start text-xl  m-4 font-bold text-white ">{rcs.title}</h3>
+              <h3 className="text-white m-4 text-start font-medium">{rcs.description}</h3>
+              <a href={rcs.link} target="_blank" rel="noopener noreferrer" className="link-btn " >
+                <Button style={{
+                  marginTop: 20, marginBottom: 20, color: "white"
+                }} >SEE MORE</Button>
+              </a>
             </div>
           ))}
         </div>
