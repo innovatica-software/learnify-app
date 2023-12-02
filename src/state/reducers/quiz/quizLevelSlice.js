3430cfb440ce9877fdd6b3100b004604fe80eaab
Nahid Hasan <nahidjc80@gmail.com>
2023-12-03 01:19:10 +0600

    get unlock quiz without token

--- src/state/reducers/quiz/quizLevelSlice.js
+++ src/state/reducers/quiz/quizLevelSlice.js
@@ -1,11 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { privateGet, privatePost } from "../../../utilities/apiCaller";
+import { privateGet, privatePost, publicGet } from "../../../utilities/apiCaller";
 
 export const fetchQuizLevels = createAsyncThunk(
   "quizLevel/fetchQuizLevels",
   async ({ token, countryId }, { rejectWithValue }) => {
     try {
       const response = await privateGet(`/country/${countryId}`, token);
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response);
+    }
+  }
+);
+export const fetchFreeQuizLevels = createAsyncThunk(
+  "quizLevel/fetchFreeQuizLevels",
+  async ({ countryId }, { rejectWithValue }) => {
+    try {
+      const response = await publicGet(`/country/unlocked-levels/${countryId}`);
       return response.data;
     } catch (err) {
       return rejectWithValue(err.response);
@@ -49,6 +60,18 @@ const quizLevelSlice = createSlice({
         state.levels = action.payload;
       })
       .addCase(fetchQuizLevels.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errorMessage = action.payload.data.message;
+      })
+      .addCase(fetchFreeQuizLevels.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchFreeQuizLevels.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.levels = action.payload;
+      })
+      .addCase(fetchFreeQuizLevels.rejected, (state, action) => {
         state.isLoading = false;
         state.errorMessage = action.payload.data.message;
       })
